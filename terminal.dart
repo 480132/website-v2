8a1bbfe7949392b2d7e14fe5cd809af1ff09997d
480132 <83782172+480132@users.noreply.github.com>
2024-02-26 08:36:28 -0800

    Create terminal.dart

---
+++ terminal.dart
@@ -0,0 +1,95 @@
+Skip to content
+480132
+/
+website-v2
+
+Type / to search
+
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Deploy Jekyll with GitHub Pages dependencies preinstalled
+Create jekyll-gh-pages.yml #1
+Jobs
+Run details
+build
+succeeded now in 41s
+Beta
+Give feedback
+Search logs
+2s
+13s
+11s
+1s
+4s
+5s
+  ./projects/1/js/local_score_manager.js
+  ./projects/1/js/tile.js
+  ./projects/1/js/game_manager.js
+  ./projects/1/js/grid.js
+  ./projects/1/js/html_actuator.js
+  ./projects/1/js/animframe_polyfill.js
+  ./projects/1/js/application.js
+  ./projects/1/js/keyboard_input_manager.js
+  ./projects/ninja/
+  ./projects/ninja/logo2.png
+  ./projects/ninja/logo1.png
+  ./projects/ninja/square.png
+  ./projects/ninja/index.html
+  ./font/
+  ./font/exo2/
+  ./font/exo2/Exo2-Regular.ttf
+  ./index.html
+  ./comments.html
+  ./favicon.ico
+  ./js/
+  ./js/particles.js
+  ./js/main.js
+  ./assets/
+  ./assets/css/
+  ./assets/css/style.css
+  ./css/
+  ./css/style.css
+  ./LICENSE
+  ./404.html
+Run actions/upload-artifact@v4
+With the provided path, there will be 1 file uploaded
+Artifact name is valid!
+Root directory input is valid!
+Beginning upload of artifact content to blob storage
+Uploaded bytes 8388608
+Uploaded bytes 16777216
+Uploaded bytes 25165824
+Uploaded bytes 33554432
+Uploaded bytes 41943040
+Uploaded bytes 50331648
+Uploaded bytes 58720256
+Uploaded bytes 67108864
+Uploaded bytes 75497472
+Uploaded bytes 83886080
+Uploaded bytes 85514055
+Finished uploading artifact content to blob storage!
+SHA256 hash of uploaded artifact zip is 41a352d3f65c871c235ceaf21b77e15ad81e36795f71581bda391f4098740be5
+Finalizing artifact upload
+Artifact github-pages.zip successfully finalized. Artifact ID 1275863200
+Artifact github-pages has been successfully uploaded! Final size is 85514055 bytes. Artifact ID is 1275863200
+Artifact download URL: https://github.com/480132/website-v2/actions/runs/8052357347/artifacts/1275863200
+0s
+Post job cleanup.
+/usr/bin/git version
+git version 2.43.2
+Temporarily overriding HOME='/home/runner/work/_temp/a9da2d7a-19ed-40be-9cae-04fed7b63fdc' before making global git config changes
+Adding repository directory to the temporary git global config as a safe directory
+/usr/bin/git config --global --add safe.directory /home/runner/work/website-v2/website-v2
+/usr/bin/git config --local --name-only --get-regexp core\.sshCommand
+/usr/bin/git submodule foreach --recursive sh -c "git config --local --name-only --get-regexp 'core\.sshCommand' && git config --local --unset-all 'core.sshCommand' || :"
+/usr/bin/git config --local --name-only --get-regexp http\.https\:\/\/github\.com\/\.extraheader
+http.https://github.com/.extraheader
+/usr/bin/git config --local --unset-all http.https://github.com/.extraheader
+/usr/bin/git submodule foreach --recursive sh -c "git config --local --name-only --get-regexp 'http\.https\:\/\/github\.com\/\.extraheader' && git config --local --unset-all 'http.https://github.com/.extraheader' || :"
+0s
